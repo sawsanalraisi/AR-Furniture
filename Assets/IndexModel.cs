162b275c275e4c6a154bf7cd95119b5f95d17be0
sawsanalraisi <44733946+sawsanalraisi@users.noreply.github.com>
2021-02-01 07:07:57 +0400

    up

---
+++ Assets/IndexModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndexModel : MonoBehaviour
+{
+
+    public int modelID;
+
+
+    public void ModelIDFun()
+    {
+
+        ManagerScript.instance.IndexModel = modelID;
+    }
+  
+}
